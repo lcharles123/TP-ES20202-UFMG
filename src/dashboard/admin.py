3feaf7d580e3e7e4a90eafb608dfae6c7527eb37
lcharles123 <tidus098@hotmail.com>
2021-01-19 21:26:59 +0000

    adicionando admin

--- src/dashboard/admin.py
+++ src/dashboard/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+#registrar os modelos para administracao do site
+
+
+
+# apenas testando o registro de um modelo contido no arquivo models.py que eh uma classe python
+
+from dashboard.models import MyModelName
+
+admin.site.register(MyModelName)
+
+
+
+
--- src/dashboard/models.py
+++ src/dashboard/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+#definindo o modelo da pagina
+class MyModelName(models.Model):
+    """Uma típica classe definindo um modelo, derivada da classe Model."""
+
+    # Campos
+    my_field_name = models.CharField(max_length=20, help_text='Enter field documentation')
+    ...
+
+    # Metadados
+    class Meta:
+        ordering = ['-my_field_name']
+
+    # Métodos
+    def get_absolute_url(self):
+        """Retorna a url para acessar uma instancia específica de MyModelName."""
+        return reverse('model-detail-view', args=[str(self.id)])
+
+    def __str__(self):
+        """ String para representar o objeto MyModelName (no site Admin)."""
+        return self.my_field_name
